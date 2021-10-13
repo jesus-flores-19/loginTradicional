03851276e294e36ea1bd8ca2d738335005e5e326
Jesus Arath <jesus.flores.p19@gmail.com>
2021-10-13 12:18:58 -0500

    Agregamos alertas y enrutamiento en componente login

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { usuarioModel } from 'src/app/models/usuario.model';
 import { AuthService } from 'src/app/services/auth.service';
+import {Router} from "@angular/router"
+import Swal from 'sweetalert2'
 
 
 @Component({
@@ -11,7 +13,7 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(public auth: AuthService) { 
+  constructor(public auth: AuthService, public router: Router) { 
     this.user = new usuarioModel();
   }
 
@@ -23,11 +25,27 @@ export class LoginComponent implements OnInit {
   onSubmit(form: NgForm){
     if(form.invalid){return}
     console.log(this.user);
+
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: "info",
+      text: "Cargando"
+    })
+    Swal.showLoading();
+
     this.auth.login(this.user).subscribe(
       (data: any) => {
         console.log(data);
+        Swal.close();
+        this.router.navigate(["home"])
       },(err: any) => {
-        console.log(err.error.error.message);
+        Swal.fire({
+          title: "Error al autenticar",
+          text: err.error.error.message,
+          icon: "error",
+          confirmButtonText: "Ok",
+        })
+        console.error(err.error.error.message);
       }
     )
     
